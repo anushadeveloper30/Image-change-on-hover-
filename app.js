a6727a79e792ab73825b557ddd81117197363791
Anusha Akhter ❤️ <akhteranusha38@gmail.com>
2026-01-21 09:50:41 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,3 @@
-// buttons => onclick , doubleclick
-// input => onfocus , onblur , onchange 
-// hover => onmouseover , onmousedown
-// notworkable => onmouseup
-//three types of buttonns => btn , submit , reset 
-// tags of buttons => <button> , <input type="button"> , <input type="image">
-
-
-// // COPY
-// document.addEventListener('copy', (e) =>{
-//     e.preventDefault();
-//     swal.fire({
-//         title:'No No plz do not copy my text',
-//         text:'chori krna ghlt bt hai ',
-//         icon:'warning'
-//     })
-// })
-
-
-// // MOUSEOVER
-// document.getElementById('para').addEventListener('mouseover', ()=>{
-//     swal.fire({
-//         title: 'Hey there!',
-//         text: 'You hovered over the paragraph!',
-//         icon: 'info'
-//     })
-// })
-
-
-// // MOUSEMOVE
-// document.getElementById('para').addEventListener('mousemove', ()=>{
-//     swal.fire({
-//         title: 'Hey there!',
-//         text: 'You hovered over the paragraph!',
-//         icon: 'info'
-//     })
-// })
 
 // IMAGE CHANGE ON HOVER
 let image = document.getElementById('image');
@@ -43,4 +6,5 @@ image.addEventListener('mouseover', ()=>{
 })
 image.addEventListener('mouseout', ()=>{
   image.src = 'image1.jpeg';
+
 });
